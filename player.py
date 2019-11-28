74feae39995f2f0812b3566813a3f9d5a8db3b5e
pedroivovasconcelos <56645430+pedroivovasconcelos@users.noreply.github.com>
2019-11-27 22:15:45 -0300

    game

--- player.py
+++ player.py
@@ -2,22 +2,33 @@ import threading, pygame, sys
 pygame.init()    
 
 black = 0, 0, 0
+        
+def shooting(shoot,shootrect):
+    while True:
+        if (shootrect.top != 0):
+            shootrect = shootrect.move(0,-5)
+            print("tiro andando")
+        else:
+            shootrect = shootrect.move(x+15,y-50)
+            print("tiro chegou")
+            return
+        screen.blit(shoot, shootrect)
+        pygame.time.delay(41)
 
 if __name__ == "__main__":
-    speed = 5
-    lives = 3
-    #tplayer = threading.Thread(target=player, args=(speed,lives))
-    #tplayer.join
-
     size = width, height = 300, 500
     screen = pygame.display.set_mode(size)
     pygame.display.set_caption('Space Invaders - 2019 ATR/UFMG')
 
-    spaceship = pygame.image.load("spaceship.png")
-    spaceshiprect = spaceship.get_rect()
     x = 125
     y = 400
+    spaceship = pygame.image.load("spaceship.png")
+    spaceshiprect = spaceship.get_rect()
     spaceshiprect = spaceshiprect.move(x,y)
+
+    shoot = pygame.image.load("shoot.png")
+    shootlist = []
+    counter = 0
 
     while 1:
         pygame.time.delay(41)
@@ -28,16 +39,28 @@ if __name__ == "__main__":
 
         key = pygame.key.get_pressed()
         if key[pygame.K_LEFT]:
-            print("left")
             if (0 != spaceshiprect.left):
                 spaceshiprect = spaceshiprect.move(-5,0)
         if key[pygame.K_RIGHT]:
-            print("right")
             if (width != spaceshiprect.right):
                 spaceshiprect = spaceshiprect.move(5,0)
         if key[pygame.K_SPACE]:
             print("space")
-            pygame.draw.rect(screen,(255,255,0),(250,250,50,50))
+            if counter == 0:
+
+                shootrect = shoot.get_rect()
+                shootrect.move(spaceshiprect.left+15,spaceshiprect.top-15)
+                shootlist.append(shootrect)
+                counter = 5
+            else:
+                counter-=1
         
+        for shoots in shootlist:
+            try:
+                shoots = shoots.move(0,-5)
+                if shoots.top < 0:
+                    shootlist.remove(shoots.top)
+            screen.blit(shoot, shoots)
+            
         screen.blit(spaceship, spaceshiprect)
         pygame.display.flip()
