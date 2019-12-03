264bde39746a2dd8a063e1f81eb8a678ec6772ca
pedroivovasconcelos <56645430+pedroivovasconcelos@users.noreply.github.com>
2019-12-03 12:59:53 -0300

    test

--- obstacles.py
+++ obstacles.py
@@ -5,28 +5,22 @@ y = 375
 damage = 0
 level = 0
 
-def renewobs(obstaclelist):
+def renewobs(gv):
+    obstaclelist = []
     obstacle = pygame.image.load("images/obstacles0.png")
     obstaclerect = obstacle.get_rect()
     for a in range(4):
-        print(a)
         obstaclelist.append([obstacle,damage,obstaclerect.move(x+a*65, obstaclerect.top+y)])
+    gv.obstaclelist = obstaclelist
 
 def obstacles(gv):
     global level
-    renewobs(gv.obstaclelist)
+    global damage
         
     while 1:
         if level != gv.level:
             level = gv.level
-            obstaclelist = []
             damage = 0
-            
-            
-        # for a in range(4):
-        #     image = "images/obstacles{}.png".format(damage)
-        #     obstacle = pygame.image.load(image)
-        #     obstaclerect = obstacle.get_rect()
-        #     obstaclelist.append([obstacle,damage,obstaclerect.move(x+a*65, obstaclerect.top+y)])
+            renewobs(gv)
         
         time.sleep(gv.fps)
